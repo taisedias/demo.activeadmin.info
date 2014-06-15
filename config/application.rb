c8a0ba4497f112cefb6b74640acb47d72f4ab4c0
Rafael <rafalima07@gmail.com>
2014-06-15 11:18:08 -0300

    removing the hourly and daily limit on the Rack Throttle config

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,8 @@ require 'rack/throttle'
 module ActiveadminDepot
   class Application < Rails::Application
 
-    config.middleware.use Rack::Throttle::Hourly, :max => 50
-    config.middleware.use Rack::Throttle::Daily,  :max => 300
+    # config.middleware.use Rack::Throttle::Hourly, :max => 50
+    # config.middleware.use Rack::Throttle::Daily,  :max => 300
 
     config.autoload_paths << "#{config.root}/lib"    # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
